5fd94fb8c75d0a41c3aad051a341c367777fe3c3
fengshi96 <fengshichn@gmail.com>
2022-07-11 20:25:19 -0400

    update

--- src_julia/sampler_B.jl
+++ src_julia/sampler_B.jl
@@ -86,28 +86,8 @@ function Svn(sysIndx::Vector{Int}, Nsite::Int, wf::Vector{ComplexF64})
     return ee, PentS, rdm
 end
 
-function ProjectZ!(ρ::Matrix{ComplexF64}, site::Int, dir::Int)
-    Nsites = Int(log2(size(ρ)[1]))
-    sz = [1 0 ; 0 -1]
-    ida = Matrix{ComplexF64}(I, 2^(site-1), 2^(site-1))
-    idb = Matrix{ComplexF64}(I, 2^(Nsites-site), 2^(Nsites-site))
-    PauliZ = kron(ida, sz, idb)
-    Id = Matrix{ComplexF64}(I, 2^Nsites, 2^Nsites)
-    
-    if dir == 1
-        ρ = 0.25 * (Id + PauliZ) * ρ * (Id + PauliZ)
-        ρ /= tr(ρ)
-        @assert tr(ρ*PauliZ) ≈ 1
-    elseif dir == -1
-        ρ = 0.25 * (Id - PauliZ) * ρ * (Id - PauliZ)
-        ρ /= tr(ρ)
-        @assert tr(ρ*PauliZ) ≈ -1
-    end
-    return ρ
-end
-
-function OneShotZ!(ρ::Matrix{ComplexF64}, site::Int, rngIndx=1234)
-    Nsites = Int(log2(size(ρ)[1]))
+function OneShotZ!(ρ::Array{Matrix{ComplexF64}}, site::Int, rngIndx=1234)
+    Nsites = Int(log2(size(ρ[1])[1]))
     rng = MersenneTwister(rngIndx)
     ida = Matrix{ComplexF64}(I, 2^(site-1), 2^(site-1))
     idb = Matrix{ComplexF64}(I, 2^(Nsites-site), 2^(Nsites-site))
@@ -115,21 +95,43 @@ function OneShotZ!(ρ::Matrix{ComplexF64}, site::Int, rngIndx=1234)
     PauliZ = kron(ida, sz, idb)
     Id = Matrix{ComplexF64}(I, 2^Nsites, 2^Nsites)
 
-    Z = tr(ρ * PauliZ)
-    @show prob_up = real(0.5 * (1 + Z))
+    Z = tr(ρ[1] * PauliZ)
+    prob_up = real(0.5 * (1 + Z))
    
-#     sample::Bool = nothing
-#     @show rnd=rand(rng)
     if prob_up > rand(rng)
         sample = 1  # up
-        ρ = 0.25 * (Id + PauliZ) * ρ * (Id + PauliZ)
-        ρ /= tr(ρ)
+        ρ[1] = 0.25 * (Id + PauliZ) * ρ[1] * (Id + PauliZ)
+        ρ[1] /= tr(ρ[1])
     else
         sample = -1  # down
-        ρ = 0.25 * (Id - PauliZ) * ρ * (Id - PauliZ)
-        ρ /= tr(ρ)
+        ρ[1] = 0.25 * (Id - PauliZ) * ρ[1] * (Id - PauliZ)
+        ρ[1] /= tr(ρ[1])
     end
-    return sample, ρ
+    return sample
+end
+
+function OneShotZ2!(ψ::Array{Vector{ComplexF64}}, site::Int, rngIndx=1234)
+    Nsites = Int(log2(size(ψ[1])[1]))
+    rng = MersenneTwister(rngIndx)
+    ida = Matrix{ComplexF64}(I, 2^(site-1), 2^(site-1))
+    idb = Matrix{ComplexF64}(I, 2^(Nsites-site), 2^(Nsites-site))
+    sz = [1 0 ; 0 -1]
+    PauliZ = kron(ida, sz, idb)
+    Id = Matrix{ComplexF64}(I, 2^Nsites, 2^Nsites)
+
+    Z = adjoint(ψ[1]) * PauliZ * ψ[1]
+    prob_up = real(0.5 * (1 + Z))
+   
+    if prob_up > rand(rng)
+        sample = 1  # up
+        ψ[1] = 0.5 * (Id + PauliZ) * ψ[1]
+        ψ[1] /= sqrt(prob_up)
+    else
+        sample = -1  # down
+        ψ[1] = 0.5 * (Id - PauliZ) * ψ[1]
+        ψ[1] /= sqrt(1 - prob_up)
+    end
+    return sample
 end
 
 let
@@ -143,36 +145,31 @@ dset=hd5["3.Eigen/Wavefunctions"]
 evec=read(dset)
 close(hd5)
 
-
-eeA, PentS_A, rdm = Svn(sysIndxA, Nsite, evec[1,:])
-# push!(ESpecA, PentS_A)
-# println(eeA)
-# show(stdout, "text/plain", rdm); println()
-
-ρ = ProjectZ!(rdm, 4, 1)  # fix site 4 to be spin-up
-numShots = 5000
-samples = Matrix{Int8}(undef, (numShots, 3))
+ψ = evec[1, :]
+# ρ = wf_as_mat * adjoint(wf_as_mat)
+_,_,ρ = Svn(sysIndxA, Nsite, evec[1,:])
+numDof = 4
+numShots = 50
+samples = Matrix{Int8}(undef, (numShots, numDof))
 for i in 1:numShots
-    rdm = ρ
-    sample4, rdm = OneShotZ!(rdm, 4, rand(100:9000)); #println(sample4)
-    sample1, rdm = OneShotZ!(rdm, 1, rand(100:9000)); #println(sample1)
-    sample2, rdm = OneShotZ!(rdm, 2, rand(100:9000)); #println(sample2)
-    sample3, rdm = OneShotZ!(rdm, 3, rand(100:9000)); #println(sample3)
-    samples[i, 1] = sample1
-    samples[i, 2] = sample2
-    samples[i, 3] = sample3
+    rdm = [ψ] # [ρ]
+    tmp = Array{Int}(undef, 0)
+    for s in 1:numDof
+        push!(tmp, OneShotZ2!(rdm, s, rand(100:9000)))
+    end
+    println(tmp)
 end
 # show(stdout, "text/plain", samples); println()
-
-io = open("SampleTC4.dat", "w")
-for i in 1 : numShots	
-    for j in 1 : 3
-        write(io, string(samples[i, j]) * " ")
-	end
-	write(io, "\n")
-end
-close(io)
-
+# 
+# io = open("SampleTC4.dat", "w")
+# for i in 1 : numShots	
+#     for j in 1 : 3
+#         write(io, string(samples[i, j]) * " ")
+# 	end
+# 	write(io, "\n")
+# end
+# close(io)
+# 
 
 end
 
