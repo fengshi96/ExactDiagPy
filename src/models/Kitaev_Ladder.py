8d0d14792915e00bb3100b491b6f99b7b5276783
Shi Feng <fenshichn@gmail.com>
2025-04-01 20:57:10 +0200

    update flux measurements

--- src/models/Kitaev_Ladder.py
+++ src/models/Kitaev_Ladder.py
@@ -11,6 +11,7 @@ class Kitaev_Ladder(Hamiltonian):
     def __init__(self, Lat, Para):
         super().__init__(Lat, Para)
 
+        self.Lx = Lat.LLX  # Lattice dimensions
         self.Hx = Para.parameters["Bxx"]
         self.Hy = Para.parameters["Byy"]
         self.Hz = Para.parameters["Bzz"]
@@ -158,25 +159,134 @@ class Kitaev_Ladder(Hamiltonian):
                 ida = sp.eye(2 ** i)
                 idb = sp.eye(2 ** (self.Nsite - i - 1))
                 if i%2 == 0:
-                    Ham += sp.kron(ida, sp.kron(self.sx, idb)) * self.Hx
-                    Ham += sp.kron(ida, sp.kron(self.sy, idb)) * self.Hy
-                    Ham += sp.kron(ida, sp.kron(self.sz, idb)) * self.Hz
+                    Ham += sp.kron(ida, sp.kron(self.sx, idb)) * self.Hx * 2
+                    Ham += sp.kron(ida, sp.kron(self.sy, idb)) * self.Hy * 2
+                    Ham += sp.kron(ida, sp.kron(self.sz, idb)) * self.Hz * 2
                 elif i%2 != 0:
-                    Ham -= sp.kron(ida, sp.kron(self.sx, idb)) * self.Hx
-                    Ham -= sp.kron(ida, sp.kron(self.sy, idb)) * self.Hy
-                    Ham -= sp.kron(ida, sp.kron(self.sz, idb)) * self.Hz
+                    Ham -= sp.kron(ida, sp.kron(self.sx, idb)) * self.Hx * 2
+                    Ham -= sp.kron(ida, sp.kron(self.sy, idb)) * self.Hy * 2
+                    Ham -= sp.kron(ida, sp.kron(self.sz, idb)) * self.Hz * 2
         return Ham
 
        
+    def buildFluxList(self):
+        """
+        Measure the fluxes in the system
+        :return: 1D list of fluxes
+        1 - x - 3 - y - 5 - x - 7 - y - 9 - x - 11 -
+        |       |       |       |       |       |            
+        z  Ws1  z  Ws2  z  Ws1  z  Ws2  z  Ws1  z              
+        |       |       |       |       |       |          
+        0 - y - 2 - x - 4 - y - 6 - x - 8 - y - 10 -
+        with 
+        Ws1 = X0 Y1 X2 Y3 + translations
+        Ws2 = Y2 X3 Y4 X5 + translations
+        Ws1.Ws2 = Wp = X0 Y1 Z2 Z3 Y4 X5 + translations
+        """
+        # Define the Ws1 operators
+        Ws1_Indx = []
+        Ws1_Ops = []
+        Ws2_Indx = []
+        Ws2_Ops = []
+        Wp_Indx = []
+        Wp_Ops = []
+        for i in np.arange(0, self.Lat.Nsite, 2):
+            print(i)
+            if i % 4 == 0:
+                # Ws1 operator with lower-left corner on sites 0, 4, 8, ...
+                j0 = i
+                j1 = self.Lat.nn_[i, 2]
+                j2 = self.Lat.nn_[i, 0] 
+                j3 = self.Lat.nn_[j1,0]
+                # print("[Ws1 sites] j0, j1, j2, j3 = ", j0, j1, j2, j3)
+                if j0 >= 0 and j1 >= 0 and j2 >= 0 and j3 >= 0:
+                    Ws1_Indx.append([j0, j1, j2, j3])
+                    Ws1_Ops.append(["x", "y", "x", "y"])  # X0 Y1 X2 Y3, the order of operators in the plaquette
+                # Wp operator with lower-left corner on sites 0, 4, 8, ...
+                j4 = self.Lat.nn_[j2, 0] 
+                j5 = self.Lat.nn_[j3, 0] 
+                # print("[Wp sites] j0, j1, j2, j3, j4, j5 = ", j0, j1, j2, j3, j4, j5)
+                if j0 >= 0 and j1 >= 0 and j2 >= 0 and j3 >= 0 and j4>= 0 and j5 >= 0:
+                    Wp_Indx.append([j0, j1, j2, j3, j4, j5])
+                    Wp_Ops.append(["x", "y", "z", "z", "y", "x"])  # X0 Y1 Z2 Z3 Y4 X5, the order of operators in the plaquette
+            
+            # Ws2 operator with lower-left corner on sites 2, 6, 10, ... 
+            elif i % 4 == 2:
+                j2 = i
+                j3 = self.Lat.nn_[i, 2]
+                j4 = self.Lat.nn_[i, 0] 
+                j5 = self.Lat.nn_[j3,0]
+                # print("[Ws2 sites] j2, j3, j4, j5 = ", j2, j3, j4, j5)
+                if j2 >= 0 and j3 >= 0 and j4 >= 0 and j5 >= 0:
+                    Ws2_Indx.append([j2, j3, j4, j5])
+                    Ws2_Ops.append(["y", "x", "y", "x"]) 
 
+        return np.array(Ws1_Indx), np.array(Ws1_Ops), np.array(Ws2_Indx), np.array(Ws2_Ops), np.array(Wp_Indx), np.array(Wp_Ops)
 
-       
-    
+                
+        
 
 if __name__ == '__main__':
     from src.Parameter import Parameter
     from src.Lattice import Lattice
+    import h5py
+    from src.Observ import matele
 
     param = Parameter("/Users/shifeng/Projects/ExactDiagPy/input.inp")
     lat = Lattice(param)
     test = Kitaev_Ladder(lat, param)
+
+
+    # test measuring fluxes 
+    outputname = "/Users/shifeng/Projects/ExactDiagPy/dataSpec.hdf5"
+    inputname = "/Users/shifeng/Projects/ExactDiagPy/input.inp"
+
+    para = Parameter(inputname)
+    Lat = Lattice(para)
+    ob = Observ(Lat, para)
+    print(para.parameters["Model"])
+    dof = Dofs("SpinHalf")  # default spin-1/2
+
+    # ------- Read dataSpec file -------
+    rfile = h5py.File(outputname, 'r')
+    evalset = rfile["3.Eigen"]["Eigen Values"]
+    evecset = rfile["3.Eigen"]["Wavefunctions"]
+
+    # extract eigen value and eigen vector from HDF5 rfile
+    evals = np.zeros(para.parameters["Nstates"], dtype=float)
+    evecs = np.zeros((dof.hilbsize ** Lat.Nsite, para.parameters["Nstates"]), dtype=complex)
+    evalset.read_direct(evals)
+    evecset.read_direct(evecs)
+    rfile.close()
+    gs = evecs[:, 0]
+
+    # Build the flux list
+    ws1_indx, ws1_ops, ws2_indx, ws2_ops, wp_indx, wp_ops = test.buildFluxList()
+    print("ws1_indx, ws1_ops = ", ws1_indx, ws1_ops)
+    print("ws2_indx, ws2_ops = ", ws2_indx, ws2_ops)
+    print("wp_indx, wp_ops = ", wp_indx, wp_ops)
+
+    # Build the flux operators and measure
+    print("measuring ws1 fluxes:")
+    ops = test.BuildFlux(ws1_indx, ws1_ops)
+    for op in ops:
+        result = matele(gs, op, gs).real
+        print(result)
+
+    print("measuring ws2 fluxes:")
+    ops = test.BuildFlux(ws2_indx, ws2_ops)
+    for op in ops:
+        result = matele(gs, op, gs).real
+        print(result)
+
+    print("measuring wp fluxes:")
+    ops = test.BuildFlux(wp_indx, wp_ops)
+    for op in ops:
+        result = matele(gs, op, gs).real
+        print(result)
+
+
+
+
+
+    
